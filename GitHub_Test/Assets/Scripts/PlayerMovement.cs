227b319b13a7fa813d5177e36ec864783d419cfc
SpinDash11 <jacobosh10@gmail.com>
2023-10-31 13:25:40 -0700

    new additions to script

---
+++ GitHub_Test/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Impliment player movement
+    public float speed;
+    private Vector3 movePos;
+
+    private void MovePlayer()
+    {
+
+    }
+
+    private void Jump()
+    {
+        //makes the player jump
+    }
+}
